ad1b3b1000ea9c6db5047acf522cf79cd46fe1c2
David-Mason <davidhmason1992@gmail.com>
2022-01-29 15:53:19 +0000

    replaced hardcoded tiles with generated tiles

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,48 +7,49 @@ interface Tile {
 export default () => {
     const [tiles, setTiles] = useState<Tile[][]>([]);
     console.log(tiles)
-    useEffect(()=> {
+    useEffect(() => {
 
-        setTiles( [
-            [{colour:'blue'}, {colour:'blue'}], 
-                [{colour:'red'}]
-                ])
+
+
+
 
         // TODO set tiles
+        type Board = Tile[][]
+
+        const generateBoard = (length: number = 10) => {
+
+            const board: Board = [];
+            const tileOptions = [{ colour: 'blue' }]
+            for (let rowIndex = 0; rowIndex < length; rowIndex++) {
+                let row = []
+                for (let tileIndex = 0; tileIndex < length; tileIndex++) {
+                    const randomElement = tileOptions[Math.floor(Math.random() * tileOptions.length)];
+                    row.push(randomElement)
+                }
+                board.push(row)
+            }
+            return board
+        }
+        const b = generateBoard();
+        console.log(b);
+
+        setTiles(b)
 
 
-        // const generateBoard = (length: number=10) => {
+    }, [])
 
-        //     const board: Board = [];
-        //     const tileOptions = ['?', '#', 'W', '0']
-        //     for (let rowIndex = 0; rowIndex < length; rowIndex++) {
-        //         let row = []
-        //         for (let tileIndex = 0; tileIndex < length; tileIndex++) {
-        //             const randomElement = tileOptions[Math.floor(Math.random() * tileOptions.length)];
-        //             row.push(randomElement)
-        //         }
-        //         board.push(row)
-        //     }
-        //     return board
-        // }
-
-
-
-
-
-    },[])
 
     return <>
-    <h1>Game</h1>
+        <h1>Game</h1>
 
-    {tiles.length !== 0 && tiles.map(row=> {
+        {tiles.length !== 0 && tiles.map(row => {
 
-        return <div>
-            {row.map(t => {
-                return <div>{t.colour}</div>
-            })}
-        </div>
-    })}
+            return <div>
+                {row.map(t => {
+                    return <div>{t.colour}</div>
+                })}
+            </div>
+        })}
 
     </>
 }
